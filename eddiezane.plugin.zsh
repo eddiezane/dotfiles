3430a08c473123ee37d8787772e733693c65e2b8
Eddie Zaneski <eddiezane@gmail.com>
2013-10-30 21:31:28 -0400

    Add SG shortcut

--- eddiezane.plugin.zsh
+++ eddiezane.plugin.zsh
@@ -2,6 +2,10 @@ DDev() { cd ~/Dropbox/Development/$1; }
 _DDev() { _files -W ~/Dropbox/Development -/; }
 compdef _DDev DDev
 
+SG() { cd ~/Code/SendGrid/$1; }
+_SG() { _files -W ~/Code/SendGrid -/; }
+compdef _SG SG
+
 __git_files () { _wanted files expl 'local files' _files }
 
 function rbenv_check() {
