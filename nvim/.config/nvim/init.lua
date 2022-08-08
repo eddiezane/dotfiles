157be7de33193f3d62fbab73f12223bf054a5ecc
Eddie Zaneski <eddiezane@gmail.com>
2022-08-08 09:59:27 -0600

    Updates
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -39,7 +39,7 @@ require('nvim-treesitter.configs').setup({
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
   indent = {
     enable = true,
