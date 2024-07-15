9490721df9eabd95ced31ab7154f163e923c7bde
Eddie Zaneski <eddiezane@gmail.com>
2024-07-15 10:22:45 -0600

    Updates
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -8,6 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.debugging.telescope-dap-nvim" },
   -- import/override with your plugins folder
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -11,4 +11,13 @@ return {
     "zbirenbaum/copilot-cmp",
     config = function() require("copilot_cmp").setup() end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {},
+  },
 }
