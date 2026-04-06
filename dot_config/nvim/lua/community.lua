ef68cec636365c9c94dabe2fcb2ab3308c6ac765
Eddie Zaneski <eddiezane@gmail.com>
2026-04-06 14:42:14 -0600

    update
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -8,8 +8,8 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.debugging.telescope-dap-nvim" },
 }
